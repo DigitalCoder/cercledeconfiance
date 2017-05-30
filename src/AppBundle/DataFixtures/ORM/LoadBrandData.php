bc5821442418c84c49690fc3e28a199fa665ef12
julien <julienmartin.opto@gmail.com>
2017-05-30 21:33:35 +0200

    fixtures userBundle ok

---
+++ src/AppBundle/DataFixtures/ORM/LoadBrandData.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: julien
+ * Date: 30/05/17
+ * Time: 21:27
+ */
+
+namespace AppBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class LoadBrandData extends AbstractFixture implements OrderedFixtureInterface
+{
+    /**
+     * Load data fixtures with the passed EntityManager
+     * @param ObjectManager $manager
+     */
+    public function load(ObjectManager $manager)
+    {
+
+    }
+
+    /**
+     * Get the order of this fixture
+     * @return integer
+     */
+    public function getOrder()
+    {
+        return 0;
+    }
+}
