37fb72e3d07f1040e7b1df7d6b136342594d7600
julien <julienmartin.opto@gmail.com>
2017-06-02 11:09:51 +0200

    add circle connection links

--- src/AppBundle/Controller/CreateCircleController.php
+++ src/AppBundle/Controller/CreateCircleController.php
@@ -19,7 +19,7 @@ use Symfony\Component\HttpFoundation\Request;
 class CreateCircleController extends Controller
 {
     /**
-     * @Route("/test")
+     * @Route("/create_circle")
      */
     public function createCircle(Request $request){
         $cercle = new Circle_user();
--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -7,9 +7,59 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class DefaultController extends Controller
 {
-
-    public function indexAction()
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("/show_circles")
+     */
+    public function showCircles()
     {
-        return $this->render('AppBundle:Default:index.html.twig');
+        return $this->render('AppBundle:Default:showCircles.html.twig');
+    }
+
+    /**
+     * @Route("/create")
+     */
+    public function createAction()
+    {
+        return $this->render('FrontBundle:Default:createCircle.html.twig');
+    }
+
+    /**
+     * @Route("/admin/objets")
+     */
+    public function adminObjetsAction()
+    {
+        if (true === $this->get('security.authorization_checker')->isGranted('ROLE_ADMIN')) {
+            return $this->render('FrontBundle:Admin:adminObjets.html.twig');
+        }
+        $error = "Erreur!";
+        $param = ['error'=>$error];
+        return $this->render('FrontBundle:Default:index.html.twig', $param);
+    }
+
+    /**
+     * @Route("/admin/services")
+     */
+    public function adminServicesAction()
+    {
+        if (true === $this->get('security.authorization_checker')->isGranted('ROLE_ADMIN')) {
+            return $this->render('FrontBundle:Admin:adminServices.html.twig');
+        }
+        $error = "Erreur!";
+        $param = ['error'=>$error];
+        return $this->render('FrontBundle:Default:index.html.twig', $param);
+    }
+
+    /**
+     * @Route("/admin/users")
+     */
+    public function adminUsersAction()
+    {
+        if (true === $this->get('security.authorization_checker')->isGranted('ROLE_ADMIN')) {
+            return $this->render('FrontBundle:Admin:adminUsers.html.twig');
+        }
+        $error = "Erreur!";
+        $param = ['error'=>$error];
+        return $this->render('FrontBundle:Default:index.html.twig', $param);
     }
 }
