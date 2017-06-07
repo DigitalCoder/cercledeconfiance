085a6064a6130344e610f0d4776aa75d702ce3e7
julien <julienmartin.opto@gmail.com>
2017-06-07 10:24:00 +0200

    symf3.3 migration

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new FOS\UserBundle\FOSUserBundle(),
             new UserBundle\UserBundle(),
             new AppBundle\AppBundle(),
+            new \Symfony\Bundle\WebServerBundle\WebServerBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
