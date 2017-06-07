23b59dee7607c4d5ba7dad65488ef257786a6a31
Necromosis <cabezasmaxime@gmail.com>
2017-06-07 12:23:41 +0200

    admin_circle can view product for buying

--- src/AppBundle/Controller/CreateModelController.php
+++ src/AppBundle/Controller/CreateModelController.php
@@ -9,6 +9,7 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\Connected_object;
+use Doctrine\Tests\Common\DataFixtures\TestEntity\User;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use AppBundle\Entity\Model;
@@ -23,9 +24,21 @@ class CreateModelController extends Controller
      */
     public function createModelAction(Request $request){
 
+        //$currentUser = $this->getUser();
+        $currentCircle = 37;
+
         $em = $this->getDoctrine()->getManager();
-        $objects = $em->getRepository('AppBundle:Connected_object')->findAll();
-//        var_dump($objects);die();
-        return $this->render('FrontBundle:Admin:adminObjets.html.twig', array("objects" => $objects));
+//        $circleUser = $em->getRepository('AppBundle:Object_entry')->findBy(array("circle_user" => $currentCircle));
+//        $objectsId = array();
+//        foreach ($circleUser as $objId){
+//            $objectsId[] = $objId->getTypeObject()->getId();
+//        }
+//        $objectWithInfo = array();
+//        foreach ($objectsId as $id){
+//            $objectWithInfo[] = $em->getRepository('AppBundle:Model')->findOneBy(array("type_object" => $id));
+//        }
+        $objectWithInfo = $em->getRepository('AppBundle:Model')->findAll();
+
+        return $this->render('FrontBundle:Admin:adminObjets.html.twig', array("objects" => $objectWithInfo));
     }
 }
