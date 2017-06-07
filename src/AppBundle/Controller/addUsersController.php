11ce999d2182029215d2176ef6f77ef46e2eb17a
Slix1 <sylvain.jan2@gmail.com>
2017-06-07 10:14:01 +0200

    creation addUsersController

---
+++ src/AppBundle/Controller/addUsersController.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sylvain
+ * Date: 06/06/17
+ * Time: 17:36
+ */
+
+namespace AppBundle\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+
+class addUsers extends Controller
+{
+    /**
+     * @Route("circle/add_users/{id}")
+     */
+    public function addUsers()
+    {
+
+    }
+}
