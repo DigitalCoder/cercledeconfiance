7c786e4e1727007c59087b2c7da91e4904a102a4
Necromosis <cabezasmaxime@gmail.com>
2017-06-28 15:47:24 +0200

    corriger

--- src/AppBundle/Repository/Object_entryRepository.php
+++ src/AppBundle/Repository/Object_entryRepository.php
@@ -13,22 +13,4 @@ use Doctrine\ORM\Query\Expr\Join;
  */
 class Object_entryRepository extends \Doctrine\ORM\EntityRepository
 {
-//    public function findByBrand(EntityManagerInterface $em){
-//
-//        $repository = $em->getRepository('AppBundle:Object_entry');
-//
-//        $query = $repository->createQueryBuilder('o')
-//            ->innerJoin('o.circle_user', 'cu', Join::WITH, 'o.circle_user = cu.id')
-//            ->innerJoin('cu.id', 'cui', Join::WITH, 'cu.id = cui.id')
-//            ->where('o.circle_user = :cuid')
-//            ->orderBy('o.id', 'DESC')
-//            ->setParameter('cuid', '40')
-//            ->getQuery();
-//
-//        $dql = 'SELECT o FROM AppBundle:Object_entry o INNER JOIN o.circle_user cu WITH o.circle_user=cu.id WHERE o.circle_user=40';
-//
-//        $result = $repository->createQueryBuilder($dql)->getQuery();
-//        var_dump($query->getResult());die();
-//        return $query->getResult();
-//    }
 }
